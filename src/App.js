aaead989848635ea07d623e6ab35d8c44f33977a
shivanikulal <shivanikulal01@gmail.com>
2023-07-27 12:24:26 +0530

    DONE

---
+++ src/App.js
@@ -0,0 +1,48 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Courses from "./Components/Courses";
+import Layout from "./Components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle"
+const App = ()=>{
+  return(
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="courses" element={<Courses />} />
+        
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default App;
---
+++ src/Components/Courses.js
@@ -0,0 +1,165 @@
+const Courses = () => {
+    return (
+      <div class="container" style={{ marginTop: "100px" }}>
+        <div class="row">
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.uU-NOFOQDNnd183qeZKmcAHaGp&pid=Api&P=0&h=180"
+                class="card-img-top"
+                alt="..."
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">C</h5>
+                <p class="card-text">
+                C is a powerful and versatile programming language, widely used for system development, embedded systems, and high-performance applications.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+  
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.FaI57trIV_hSDEILtXUUrwHaFY&pid=Api&P=0&h=180"
+                class="card-img-top"
+                alt="...loading"
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">C++</h5>
+                <p class="card-text">
+                C++ is an object-oriented programming language that extends the capabilities of C,  for complex software development.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+          <br></br>
+  
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.uXkvq_VxkoNFexUGR6c2vQHaEo&pid=Api&P=0&h=180"
+                class="card-img-top"
+                alt="nothing"
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">JAVA</h5>
+                <p class="card-text">
+                Java is a popular and versatile programming language java is useful used for building diverse applications and software solutionsbuilding diverse applications and software solutio..
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+          <br>
+          </br>
+  
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeR60xzpNQFaluypjEb405WezwB5Exmx8MLw&usqp=CAU"
+                class="card-img-top"
+                alt="..."
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">Python</h5>
+                <p class="card-text">
+                Python is a high-level programming language known for its simplicity, making it ideal for various applications.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://thumbs.dreamstime.com/b/person-programmer-working-pc-laptop-program-code-screen-coding-programming-vector-concept-person-programmer-working-107767154.jpg"
+                class="card-img-top"
+                alt="nothing"
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">React</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://www.bootstrapdash.com/wp-content/uploads/2017/08/bootstrap-4-beta-whats-new.jpg"
+                class="card-img-top"
+                alt="nothing"
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">Bootstrap</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://tse2.mm.bing.net/th?id=OIP.I86mqnnCbEHKDwQNLrg6ZQHaDt&pid=Api&P=0&h=180"
+                class="card-img-top"
+                alt="nothing"
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">JS AND CSS</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://community-cdn-digitalocean-com.global.ssl.fastly.net/vUuUtKqDrY9gAbaD2czK9KrN"
+                class="card-img-top"
+                alt="nothing"
+              ></img>
+              <div class="card-body">
+                <h5 class="card-title">HTML</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Know more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  export default Courses;
